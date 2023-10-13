f239f3fb1693bd8654bbef0f299b5bf0cc75fd04
VasiliyMatr <vasiliymatrenin@gmail.com>
2023-10-13 17:00:40 +0300

    [assembler] Add assembler app main
    - Replace lexer.cpp with test main

---
+++ assembler/src/assembler.cpp
@@ -0,0 +1,22 @@
+
+#include <fstream>
+#include <iostream>
+#include <cassert>
+
+#include <shrimp/assembler.hpp>
+
+int yyFlexLexer::yywrap() {
+    return 1;
+}
+
+using namespace shrimp;
+
+int main() {
+    assembler::Assembler assembler{};
+
+    std::ofstream out {"a.out", std::ios_base::binary};
+
+    assembler.assemble(out);
+
+    return 0;
+}
